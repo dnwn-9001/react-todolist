ca3f2c2c93614a2b5e8a3aa384eb74e60d2a6907
dnwn-9001 <j10511@hanmail.net>
2024-11-04 23:48:43 +0900

    ✨working list 컴포넌트 구현

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,12 +1,37 @@
-const List = () => {
+const List = ({ listData, onClickStatusBtn }) => {
   return (
     <div>
       <p>Working</p>
       <div>
         <ul>
-          <li>할 일 1</li>
-          <li>할 일 2</li>
-          <li>할 일 3</li>
+          {listData.map((v, i) =>
+            !v.isDone ? (
+              <li key={`isNotDone_${i}`}>
+                <div>
+                  <p>{v.title}</p>
+                  <p>{v.content}</p>
+                </div>
+                <button onClick={() => onClickStatusBtn(i)}>완료</button>
+              </li>
+            ) : (
+              ""
+            )
+          )}
+        </ul>
+        <ul>
+          {listData.map((v, i) =>
+            v.isDone ? (
+              <li key={`isDone_${i}`}>
+                <div>
+                  <p>{v.title}</p>
+                  <p>{v.content}</p>
+                </div>
+                <button onClick={() => onClickStatusBtn(i)}>취소</button>
+              </li>
+            ) : (
+              ""
+            )
+          )}
         </ul>
       </div>
     </div>
