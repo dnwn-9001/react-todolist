ee73a4b73e86106668f6c5222d4a46767ca2df78
dnwn-9001 <j10511@hanmail.net>
2024-11-04 23:49:52 +0900

    ✨컴포넌트 부모로서 다양한 상태를 종합적으로 관리할 컨테이너 구현

--- src/containers/mainContainer/MainContainer.js
+++ src/containers/mainContainer/MainContainer.js
@@ -1,10 +1,46 @@
+import React, { useState } from "react";
 import { Input, List } from "../../components";
 
 const MainContainer = () => {
+  const [titleData, setTitleData] = useState("");
+  const [contentData, setContentData] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+
+  const onChangeTitleHandler = (e) => {
+    setTitleData(e.target.value);
+  };
+
+  const onChangeContentHandler = (e) => {
+    setContentData(e.target.value);
+  };
+
+  const onAddClickHandler = (e) => {
+    e.preventDefault();
+    setToDoList((prev) => [
+      ...prev,
+      { title: titleData, content: contentData, isDone: false },
+    ]);
+    setTitleData("");
+    setContentData("");
+  };
+
+  const onStatusClickHandler = (index) => {
+    setToDoList((prev) =>
+      prev.map((item, i) =>
+        i === index ? { ...item, isDone: !item.isDone } : item
+      )
+    );
+  };
   return (
     <div>
-      <Input />
-      <List />
+      <Input
+        titleVal={titleData}
+        contentVal={contentData}
+        onChangeTitle={onChangeTitleHandler}
+        onChangeContent={onChangeContentHandler}
+        onClick={onAddClickHandler}
+      />
+      <List listData={toDoList} onClickStatusBtn={onStatusClickHandler} />
     </div>
   );
 };
