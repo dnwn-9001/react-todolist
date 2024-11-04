0d322039bdefaa2efdfc18b732417e6d9b0ba8cc
dnwn-9001 <j10511@hanmail.net>
2024-11-04 23:48:19 +0900

    ✨input 컴포넌트 구현

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,15 +1,31 @@
 import React from "react";
 
-const Input = () => {
+const Input = ({
+  onClick,
+  onChangeTitle,
+  onChangeContent,
+  titleVal,
+  contentVal,
+}) => {
   return (
-    <form>
+    <form onSubmit={onClick}>
       <div>
         <sapn>제목</sapn>
-        <input />
+        <input
+          type="text"
+          placeholder="제목을 입력해주세요."
+          onChange={onChangeTitle}
+          value={titleVal}
+        />
       </div>
       <div>
         <sapn>내용</sapn>
-        <input />
+        <input
+          type="text"
+          placeholder="내용을 입력해주세요."
+          onChange={onChangeContent}
+          value={contentVal}
+        />
       </div>
       <button type="submit">추가하기</button>
     </form>
