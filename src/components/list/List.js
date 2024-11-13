5d8f3eefc21a0ca2056262dfd43f2dc2fa001ade
dnwn-9001 <j10511@hanmail.net>
2024-11-13 21:08:04 +0900

    🎨삼항연사자 코드 개선

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -25,59 +25,57 @@ const List = ({
 
       <div className={styles.working__div}>
         <ul className={styles.working__ul}>
-          {listData.map((v, i) =>
-            !v.isDone ? (
-              <li className={styles.working__ul__li} key={`isNotDone_${i}`}>
-                <div className={styles.working__ul__li__content}>
-                  <h4>{v.title}</h4>
-                  <p>{v.content}</p>
-                </div>
-                <div>
-                  <button
-                    className={styles.btn__delete}
-                    onClick={() => onClickDeleteBtn(i)}
-                  >
-                    삭제
-                  </button>
-                  <button
-                    className={styles.btn__incomplete}
-                    onClick={() => onClickStatusBtn(i)}
-                  >
-                    완료
-                  </button>
-                </div>
-              </li>
-            ) : (
-              ""
-            )
+          {listData.map(
+            (v, i) =>
+              !v.isDone && (
+                <li className={styles.working__ul__li} key={`isNotDone_${i}`}>
+                  <div className={styles.working__ul__li__content}>
+                    <h4>{v.title}</h4>
+                    <p>{v.content}</p>
+                  </div>
+                  <div>
+                    <button
+                      className={styles.btn__delete}
+                      onClick={() => onClickDeleteBtn(i)}
+                    >
+                      삭제
+                    </button>
+                    <button
+                      className={styles.btn__incomplete}
+                      onClick={() => onClickStatusBtn(i)}
+                    >
+                      완료
+                    </button>
+                  </div>
+                </li>
+              )
           )}
         </ul>
         <ul className={styles.working__ul}>
-          {listData.map((v, i) =>
-            v.isDone ? (
-              <li className={styles.working__ul__li} key={`isDone_${i}`}>
-                <div className={styles.working__ul__li__content}>
-                  <h4>{v.title}</h4>
-                  <p>{v.content}</p>
-                </div>
-                <div>
-                  <button
-                    className={styles.btn__delete}
-                    onClick={() => onClickDeleteBtn(i)}
-                  >
-                    삭제
-                  </button>
-                  <button
-                    className={styles.btn__complete}
-                    onClick={() => onClickStatusBtn(i)}
-                  >
-                    취소
-                  </button>
-                </div>
-              </li>
-            ) : (
-              ""
-            )
+          {listData.map(
+            (v, i) =>
+              v.isDone && (
+                <li className={styles.working__ul__li} key={`isDone_${i}`}>
+                  <div className={styles.working__ul__li__content}>
+                    <h4>{v.title}</h4>
+                    <p>{v.content}</p>
+                  </div>
+                  <div>
+                    <button
+                      className={styles.btn__delete}
+                      onClick={() => onClickDeleteBtn(i)}
+                    >
+                      삭제
+                    </button>
+                    <button
+                      className={styles.btn__complete}
+                      onClick={() => onClickStatusBtn(i)}
+                    >
+                      취소
+                    </button>
+                  </div>
+                </li>
+              )
           )}
         </ul>
       </div>
